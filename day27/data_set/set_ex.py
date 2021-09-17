4b0974a739a684b557ffaffc7600f53ec2cabcdb
yyz2999@naver.com <yyz2999@naver.com>
2021-09-17 13:19:36 +0900

    day28 소스파일

--- day27/data_set/set_ex.py
+++ day27/data_set/set_ex.py
@@ -13,8 +13,11 @@ print(s)
 s.remove(8)
 print(s)
 
-str = set('Hello')
+str = set('Hello')  #list()
 print(str)
 
 name = set(['콩쥐', '팥쥐', '콩쥐'])
 print(name)
+
+print(list([1, 2, 3]))
+print(set([1,2,2,3,3,3]))
